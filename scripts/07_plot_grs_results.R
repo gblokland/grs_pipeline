f1b99ce5cbea5107b1ad1fa6df08074d3c5b9d27
gblokland <gblokland@users.noreply.github.com>
2026-01-12 12:04:21 +0100

    Update 07_plot_grs_results.R

--- scripts/07_plot_grs_results.R
+++ scripts/07_plot_grs_results.R
@@ -12,19 +12,24 @@ out_prefix <- args[2]
 dat <- read.csv(input)
 
 # Expect columns: IID, GRS (from the postprocess step)
-if (!"GRS" %in% colnames(dat)) {
-  stop("Input file must contain a column named GRS")
+if (!"GRS_raw" %in% colnames(dat)) {
+  stop("Input file must contain a column named GRS_raw")
+}
+
+# Expect columns: IID, GRS (from the postprocess step)
+if (!"GRS_z" %in% colnames(dat)) {
+  stop("Input file must contain a column named GRS_raw")
 }
 
 png(paste0(out_prefix, "_histogram.png"), width=800, height=600)
-hist(dat$GRS,
+hist(dat$GRS_raw,
      main="Distribution of Genetic Risk Scores",
      xlab="GRS",
      ylab="Frequency")
 dev.off()
 
 png(paste0(out_prefix, "_boxplot.png"), width=800, height=600)
-boxplot(dat$GRS,
+boxplot(dat$GRS_raw,
         main="Genetic Risk Score Boxplot",
         ylab="GRS")
 dev.off()
